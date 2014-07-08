301ed419d0bc2a3d8b2024c2767b31739e7e08f7
Dmitry Maganov <vonagam@gmail.com>
2014-07-08 18:29:32 +0700

    add gallery_images to catering

--- app/models/catering.rb
+++ app/models/catering.rb
@@ -4,4 +4,7 @@ class Catering
 
   embeds_many :dishes
   accepts_nested_attributes_for :dishes
+
+  embeds_many :images, as: :galleryimages, class_name: 'GalleriesImage'
+  accepts_nested_attributes_for :images
 end
