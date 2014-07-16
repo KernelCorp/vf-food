024eef7840c5ceb387e67227975a48711bf30fd9
aleksey <saleksey92@gmail.com>
2014-07-16 14:55:50 +0700

    Copy part code for app controller from mygeneitcs. Added welcome_controller for root path. #vf-11 in progress

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  respond_to :html, :json
+
+  def current_user
+    super || current_admin_user
+  end
+
+  def not_found
+    raise ActionController::RoutingError.new('Error 404: Page not found')
+  end
+
 end
