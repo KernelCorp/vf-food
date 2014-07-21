d6342c2216be6c6a09237284404d0b3a0942f494
Zamyatin-AA <stekbox@gmail.com>
2014-07-21 12:43:43 +0700

    #vf-2 Fixed; added spec for about controller.

--- spec/controllers/about_controller_spec.rb
+++ spec/controllers/about_controller_spec.rb
@@ -2,4 +2,15 @@ require 'rails_helper'
 
 RSpec.describe AboutController do
 
+  before do
+    @about = create :about
+  end
+
+  describe 'GET index' do
+    it 'find abouts' do
+      get :index
+      expect(assigns(:about)).to eq [@about]
+    end
+  end
+
 end
--- spec/factories/abouts.rb
+++ spec/factories/abouts.rb
@@ -1,6 +1,5 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
   factory :about do
+    sequence(:name) {|n| "about #{n}"}
   end
 end
