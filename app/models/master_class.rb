31dbe066cb2afa04f9526cd04c8ff2d8e6c25c14
aleksey <saleksey92@gmail.com>
2014-07-21 15:09:20 +0700

    Master class page #vf-12 fixed work 5h

--- app/models/master_class.rb
+++ app/models/master_class.rb
@@ -1,4 +1,22 @@
 class MasterClass
   include Mongoid::Document
+  include Mongoid::Paperclip
   include ActsAsPage
+
+  has_mongoid_attached_file :attachment,
+      # :processors => [:thumbnail],
+      styles: {
+          thumb: {
+              geometry: '400x200',
+              # watermark_path: "#{Rails.root}/public/images/watermark.png",
+              # position: 'SouthWest'
+          }
+      },
+      convert_options:{
+          thumb: '-quality 100 -strip'
+      },
+      path: ':rails_root/public/system/images/:style/:filename',
+      url: '/system/images/:style/:filename'
+
+  validates_attachment_content_type :attachment, content_type: %w(image/jpg image/jpeg image/png)
 end
