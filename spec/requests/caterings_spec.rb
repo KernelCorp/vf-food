069e11e1e47f53f81656ecb839722c259cab7a8d
max <mk@kernel-corp.com>
2014-07-23 19:27:20 +0700

    refactoring spec

--- spec/requests/caterings_spec.rb
+++ spec/requests/caterings_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Caterings', :type => :request do
+RSpec.describe 'Caterings', type: :request do
   describe 'GET /caterings' do
     it 'Catering empty page' do
       get caterings_path
@@ -11,42 +11,44 @@ RSpec.describe 'Caterings', :type => :request do
   describe 'GET some catering' do
     before do
       5.times{ create(:catering) }
+      @first = Catering.order_by(:name.asc).first
+      get catering_path(@first)
     end
-    it 'Show first catering' do
-      first = Catering.order_by(:name.asc).first
-      get catering_path(first)
+    context 'Show first catering' do
       # Show page contain header and description
-      assert_select '#caterigns .col-sm-7 .h1', text: first.name
-      assert_select '#caterigns .col-sm-7 .description', html: first.text
+      it { assert_select '#caterigns .col-sm-7 .h1', text: @first.name }
+      it { assert_select '#caterigns .col-sm-7 .description', html: @first.text }
     end
 
-    it 'First catering have other catering links' do
-      list = Catering.order_by(:name.asc).all
-      first = Catering.order_by(:name.asc).first
-      get catering_path(first)
-      list.each do |catering|
-        assert_select '#caterigns ul.menu li a', text: catering.name
-        assert_select '#caterigns ul.menu li a', href: catering_path(catering)
+    context 'First catering have other catering links' do
+      before :each do
+        @list = Catering.order_by(:name.asc).all
+      end
+      it do
+        @list.each do |catering|
+          assert_select '#caterigns ul.menu li a', text: catering.name
+          assert_select '#caterigns ul.menu li a', href: catering_path(catering)
+        end
       end
     end
 
-    it 'Catering menu' do
-      first = Catering.order_by(:name.asc).first
-      get catering_path(first)
-      menu = first.menu
-      assert_select '#caterigns .menu_block .menu_info .h3', text: menu.name
-      assert_select '#caterigns .menu_block .menu_info .description', html: menu.description
-      assert_select '#caterigns .menu_block .menu_info .price',
-                    text: 'Стоимость: ' + menu.price + ' рублей'
+    describe 'Catering menu' do
+      before :each do
+        @menu = @first.menu
+      end
+      it { assert_select '#caterigns .menu_block .menu_info .h3', text: @menu.name }
+      it { assert_select '#caterigns .menu_block .menu_info .description', html: @menu.description }
+      it { assert_select '#caterigns .menu_block .menu_info .price',
+                          text: 'Стоимость: ' + @menu.price + ' рублей'}
     end
 
-    it 'Catering menu sections' do
-      first = Catering.order_by(:name.asc).first
-      get catering_path(first)
-      first.menu.sections.each do |section|
-        assert_select '#caterigns .menu_block .menu_section .title', text: section.name
-        assert_select '#caterigns .menu_block .menu_section .text', html: section.text
-      end
+    describe 'Catering menu sections' do
+      it {
+        @first.menu.sections.each do |section|
+          assert_select '#caterigns .menu_block .menu_section .title', text: section.name
+          assert_select '#caterigns .menu_block .menu_section .text', html: section.text
+        end
+      }
     end
   end
 
